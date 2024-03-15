bfa63af98ae530b3b2fcfa56dfbd1663fadbd21e
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-03-15 18:45:41 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-import express from "express"; //package imports
-import dotenv from "dotenv";   //package imports
+import  path  from "path";
+import express from "express"; 
+import dotenv from "dotenv";   
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";        //file imports
@@ -12,6 +13,8 @@ import {app, server} from "./socket/socket.js"
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());  //allows to extract fields such fullName and other like in auth.controller.js
@@ -23,11 +26,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-//  app.get("/", (req, res) => {
-//      res.send("Hello World!");
-//  });
+//static middleware
+app.use(express.static(path.join(__dirname,"/frontend/dist"))); //is used to connect frontend application from our server 
 
-//server
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"));
+})
+ 
+
 server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`);
