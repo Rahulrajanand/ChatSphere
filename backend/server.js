46f12c3fca7ec275cd5e61ae02dcf41b974372ce
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-04-02 10:36:30 +0530

    cron.js added

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,15 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js"; //file imports
 import {app, server} from "./socket/socket.js"
 
+import job from "./cron.js";
+
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
 dotenv.config();
+job.start();
 
 app.use(express.json());  //allows to extract fields such fullName and other like in auth.controller.js
                           // or to parse incoming requests with JSON payloads (from req.body)
